012e5560e38e1d6e91e12c1cb2fca1328756ad88
unknown <byampols@alumni.cmu.edu>
2021-09-01 16:27:25 -0400

    commented js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-//GLOBAL VARIABLES
+//GLOBAL VARIABLES//
 var startTime = 9;
 var endTime = 17;
 
 var tasks = [];
 
-//FUNCTIONS
+//FUNCTIONS//
 
 //generate an empty tasks array
 var generateTasks = function() {
@@ -20,11 +20,15 @@ var generateTasks = function() {
     }
 }
 
-//generate a time block
+//generates a time block
 var generateTime = function(time, text) {
+    //find the container
     var containerEl = $(".container");
+    //create a new row
     var rowDiv = $("<div>").attr("id", time).addClass("row time-block");
+    //initialize the time stamp
     var timeStamp = "";
+    //set the time stamp based on the time, converting from 24 hour clock to 12 hour clock
     if (time === 0) {
         timeStamp = `${time + 12}AM`;
     } else if (time < 12) {
@@ -34,9 +38,11 @@ var generateTime = function(time, text) {
     } else {
         timeStamp = `${time - 12}PM`;
     }
+    //create the span, p, and button elements
     var timeSpan = $("<span>").addClass("hour col-1 text-right py-3 px-1").text(timeStamp);
     var textP = $("<p>").addClass("description col-10 text-left py-2 px-2 mb-0").text(text);
     var button = $("<button>").addClass("btn saveBtn col-1").html('<i class="fas fa-save"></i>');
+    //append them to the row, and audit the new task, then append them to the container
     rowDiv.append(timeSpan,textP,button);
     auditWorkDay(rowDiv);
     containerEl.append(rowDiv);
@@ -60,11 +66,15 @@ var saveTasks = function() {
 
 //print date at top of screen
 var loadDate = function() {
+    //get date element in the header
     var currentDayEl = $("header").find("#currentDay");
+    //get the current date
     var date = moment().format("dddd, MMMM Do");
+    //swap the text
     currentDayEl.text(date);
 }
 
+//audits a task and applies a style based on the current time
 var auditWorkDay = function (taskEl) {
     //get time (hour) from id
     var time = parseInt($(taskEl).attr("id"));
@@ -87,7 +97,9 @@ var auditWorkDay = function (taskEl) {
     }
 }
 
-//EVENT LISTENERS
+//EVENT LISTENERS//
+
+//listen for the click on the paragraph to edit it
 $(".container").on("click", "p", function() {
     //get current text and current parent
     var text = $(this).text().trim();
@@ -100,6 +112,7 @@ $(".container").on("click", "p", function() {
     textInput.trigger("focus");
 });
 
+//listen for the click on the save button to save it
 $(".container").on("click", ".saveBtn", function() {
     //get textarea
     var textArea = $(this).parent().find("textarea");
@@ -121,13 +134,18 @@ $(".container").on("click", ".saveBtn", function() {
     auditWorkDay($(this).parent());
 });
 
+//FUNCTION CALLS//
 loadWorkDay();
 loadDate();
 
+//TIMED FUNCTION CALLS//
+
+//every five seconds, audit each of the time-blocks
 setInterval(function() {
     $(".container .time-block").each(function(index, el) {
             auditWorkDay(el);
         });
     }, 1000 * 5);
 
+//every minute update the date
 setInterval(loadDate, 1000 * 60);
